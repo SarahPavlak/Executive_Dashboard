711fde32280bafa341fa99a858d10107c508c899
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-02-15 22:37:17 -0500

    Update Executive_Dashboard.py
    added additional products for fun

--- Executive_Dashboard.py
+++ Executive_Dashboard.py
@@ -19,7 +19,8 @@ def month_lookup(month):
 	month_input={'01':'January','02':'February','03':'March','04':'April',
 	'05':'May','06':'June','07':'July','08':'August','09':'September','10':'October',
 	'11':'November', '12':'December'}
-	return month_input[month] #month_lookup adapted from hiep's github
+	return month_input[month] 
+        #month_lookup adapted from hiep's github
 
 sales = [
     'sales-201710.csv','sales-201711.csv','sales-201712.csv', 'sales-201801.csv', 'sales-201802.csv', 'sales-201803.csv', 'sales-201804.csv', 'sales-201805.csv', 'sales-201806.csv', 'sales-201807.csv', 'sales-201808.csv', 'sales-201809.csv', 'sales-201810.csv', 'sales-201811.csv', 'sales-201812.csv', 'sales-201901.csv', 'sales-201902.csv', 'sales-201903.csv', 'sales-201904.csv' #finish putting in csvs
@@ -41,7 +42,9 @@ if user_input in sales:
 
         r = request.urlopen('https://raw.githubusercontent.com/SarahPavlak/Executive_Dashboard/master/data/' + str(user_input)).read().decode('utf8').split("\n") 
         reader = csv.DictReader(r) 
-        #above r code adapted from: https://stackoverflow.com/questions/51351804/extract-csv-file-from-github-library-with-python
+            
+            #code adapted from: https://stackoverflow.com/questions/51351804/extract-csv-file-from-github-library-with-python
+
         for row in reader:
             d = dict(row)
             d = {"date": row["date"], "product": row["product"], "unit price": float(row["unit price"]), "units sold": row["units sold"], "sales price": row["sales price"]}
@@ -74,20 +77,37 @@ if user_input in sales:
             #code adapted from: https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
             #code reverse true: https://stackoverflow.com/questions/17295060/sort-the-top-ten-results 
 
-
         print("-----------------------------------------------------")
         price_usd = "{0: .2f}".format(total)
         top_product = "{0: .2f}".format((productsbysales[0][1]))
         second_product = "{0: .2f}".format((productsbysales[1][1]))
-        third_product = "{0: .2f}".format((productsbysales[2][1]))
+        third_product = "{0: .2f}".format((productsbysales[2][1])) 
+        fourth_product = "{0: .2f}".format((productsbysales[3][1])) 
+        fifth_product = "{0: .2f}".format((productsbysales[4][1])) 
+        sixth_product = "{0: .2f}".format((productsbysales[5][1])) 
+        seventh_product = "{0: .2f}".format((productsbysales[6][1])) 
+        eigth_product = "{0: .2f}".format((productsbysales[7][1])) 
+        ninth_product = "{0: .2f}".format((productsbysales[8][1])) 
+        tenth_product = "{0: .2f}".format((productsbysales[9][1])) 
+
         print ("Total Monthly Sales: " + "$" + str(price_usd)) 
         print("-----------------------------------------------------")
         print("Top Selling Products: ")
         print("1) " + productsbysales[0][0] + " $" + str(top_product))
         print("2) " +productsbysales[1][0] + " $" + str(second_product))
         print("3) " +productsbysales [2][0] + " $" + str(third_product))
-    
-            #code learning: https://realpython.com/python-lists-tuples/#python-tuples 
+        print("-----------------------------------------------------")
+        print("Additional Products: ")
+        print("4) " +productsbysales [3][0] + " $" + str(fourth_product))
+        print("5) " +productsbysales[4][0] + " $" + str(fifth_product))
+        print("6) " +productsbysales [5][0] + " $" + str(sixth_product))
+        print("7) " +productsbysales[6][0] + " $" + str(seventh_product))
+        print("8) " +productsbysales [7][0] + " $" + str(eigth_product))
+        print("9) " +productsbysales[8][0] + " $" + str(ninth_product))
+        print("10) " +productsbysales [9][0] + " $" + str(tenth_product))
+
+            #code idea: https://stackoverflow.com/questions/4800811/accessing-a-value-in-a-tuple-that-is-in-a-list
+            #code idea: https://realpython.com/python-lists-tuples/#python-tuples 
 
         print("-----------------------------------------------------")
         print("Generating Bar Chart with Business Insights...")
@@ -98,22 +118,23 @@ if user_input in sales:
         for i in range(0, len(bar_data)):
             x.append(bar_data[i]['Product'])
             y.append(bar_data[i]['Revenue USD'])
+
         data = [go.Bar(
                     x=x,
-                    y=y
+                    y=y,
             )]
         layout = go.Layout(title='Product Profits ' + str(month_lookup(month)) + " " + year,
             xaxis = dict(title="Item"),
-            yaxis = dict(title="Sales (USD)"), #source: https://github.com/s2t2/exec-dash-starter-py/blob/master/monthly_sales.py 
-            margin= go.layout.Margin(l=150, pad=8) #source: https://github.com/s2t2/exec-dash-starter-py/blob/master/monthly_sales.py 
+            yaxis = dict(title="Sales (USD)"), 
+            margin= go.layout.Margin(l=150, pad=8) 
             )
+            #code adapted from: https://github.com/s2t2/exec-dash-starter-py/blob/master/monthly_sales.py 
+
         figure = go.Figure(data = data,layout=layout)
         py.offline.plot(figure, filename='basic-bar.html', auto_open = True)
-        #bar data code adapted from madeline's shared class version
-        #note: couldnt figure out how to get bar chart axis to go to 2nd decimal place
+
+            #bar code adapted from: madeline's shared class version
+            #note: couldnt figure out how to get bar chart axis to go to 2nd decimal place
 
 else: print("Oh no! That's not a csv option! The program will now gracefully close.") 
-exit  
-
- 
- 
+exit 
